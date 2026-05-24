bbf3160cd530a8080e8cefef750d69b7eb8288c9
Manish Shrivastav <carttoshoppe@gmail.com>
2026-05-23 23:56:12 -0500

    test

--- 03-git/test-node-app/app/server.js
+++ 03-git/test-node-app/app/server.js
@@ -15,8 +15,6 @@ logger.info('another line');
 logger.info('This never stops');
 logger.info('Logging logging all the way');
 logger.info('I think this is enough');
-logger.info('nope, one more!');
-logger.info('log entry for remote');
 
 app.listen(3000, function () {
     logger.info("app listening on port 3000!");
